cb1dd7f55524fc2e1f51befa39b038d29cab782d
Anto R <antragha@in.ibm.com>
2020-12-01 12:38:53 +0530

    fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,11 @@ import { HomeService } from './home.service'
 export class HomeComponent implements OnInit {
   message:any
   constructor( private homeService: HomeService) {
-
+      console.log("Home Component constructor")
    }
 
   ngOnInit(): void {
+    console.log("Home Component ngOnInit")
     this.message = this.homeService.getMessage().subscribe(
       data => {
          this.message = data
